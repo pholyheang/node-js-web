c57d1dd9a3f6784c52b119e7122c6ecd75f87ba1
Lyheang <34545633+pholyheang@users.noreply.github.com>
2020-03-12 13:29:32 +0700

    add

---
+++ app.js
@@ -0,0 +1,108 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+// add graphql
+var graphqlHTTP = require('express-graphql');
+var { buildSchema } = require('graphql');
+var mysql = require('mysql');
+
+// import file routes
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var blogRouter = require('./routes/blog');
+// end import file routes
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// add lib bootstrap and jQuery
+app.use('/', express.static(__dirname + '/www')); // redirect root
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
+// end lib bootstrap and jQuery
+
+// load routes
+app.use('/', indexRouter);
+app.use('/blog', blogRouter);
+app.use('/blog/add', blogRouter);
+app.use('/blog/delete', blogRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+app.use((req, res, next) => {
+  req.mysqlDb = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : '',
+    database : 'codeigniter_db'
+  });
+  req.mysqlDb.connect();
+  next();
+}); 
+
+var schema = buildSchema(`
+  type User {
+    id: String
+    fname: String
+    email: String
+  }
+  type Query {
+    getUsers: [User],
+    getUserInfo(id: Int) : User
+    updateUserInfo(id: Int, fname: String, email: String): Boolean
+    createUser(fname: String, email: String): Boolean
+    deleteUser(id: Int): Boolean
+  }
+`);
+
+const queryDB = (req, sql, args) => new Promise((resolve, reject) => {
+    req.mysqlDb.query(sql, args, (err, rows) => {
+        if (err)
+            return reject(err);
+        rows.changedRows || rows.affectedRows || rows.insertId ? resolve(true) : resolve(rows);
+    });
+});
+
+var root = {
+  getUsers: (args, req) => queryDB(req, "select * from user").then(data => data),
+  getUserInfo: (args, req) => queryDB(req, "select * from user where id = ?", [args.id]).then(data => data[0]),
+  updateUserInfo: (args, req) => queryDB(req, "update user SET ? where id = ?", [args, args.id]).then(data => data),
+  createUser: (args, req) => queryDB(req, "insert into user SET ?", args).then(data => data),
+  deleteUser: (args, req) => queryDB(req, "delete from user where id = ?", [args.id]).then(data => data)
+};
+
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+
+module.exports = app;
---
+++ routes/blog.js
@@ -0,0 +1,75 @@
+var express = require('express');
+var router = express.Router();
+var fetch = require('node-fetch');
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+	const url = `http://localhost:4000/graphql`;
+	const query = `
+	  	query {
+	    	getUsers {
+	    		id,
+			    fname,
+			    email
+			}
+	  	}`;
+	  	const opts = {
+		  method: "POST",
+		  headers: { "Content-Type": "application/json" },
+		  body: JSON.stringify({ query })
+		};
+
+	fetch(url, opts)
+	.then(response => response.json())
+	.then(data => {
+		res.render('blog', { title: data.data.getUsers });
+		console.log('Here is the data: ', data.data); 
+	});
+});
+
+router.post('/add',function(req,res){
+	const url = `http://localhost:4000/graphql`;
+	const student = {
+        first : req.body.fname,
+        last : req.body.lname
+    }
+
+	const query = 'query { createUser(fname: "'+student.first+'", email: "'+student.last+'")}';
+	const opts = {
+		  method: "POST",
+		  headers: { "Content-Type": "application/json" },
+		  body: JSON.stringify({ query })
+		};
+    
+    fetch(url, opts)
+	.then(response => response.json())
+	.then(data => {
+		res.render('blog', { title: [] });
+		console.log('Here is the data:');
+	});
+
+    console.log(student);   
+     // res.send('respond with a resource', student.first);     
+});
+router.post('/delete',function(req,res){
+	const url = `http://localhost:4000/graphql`;
+	const student = {
+        id : req.body.id
+    }
+    const query = 'query { deleteUser(id:'+student.id+') }';
+    const opts = {
+		  method: "POST",
+		  headers: { "Content-Type": "application/json" },
+		  body: JSON.stringify({ query })
+		};
+    
+    fetch(url, opts)
+	.then(response => response.json())
+	.then(data => {
+		res.render('blog', { title: [] });
+		console.log('Here is the data:', data);
+	});
+	console.log(student.id)
+});
+
+module.exports = router;
