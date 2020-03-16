8140a03bc462aad1dac50c371c22a1010ffdc671
Lyheang <34545633+pholyheang@users.noreply.github.com>
2020-03-16 17:08:55 +0700

    update

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -2,26 +2,33 @@ $(document).ready(function() {
 	// $(".child ul").hide();
 	$(".child > a").click(function(){
 	   $(this).parent().find( "ul" ).slideToggle();
+		$(this).find(".c1").toggleClass("addsvg1");
+		$(this).find(".c2").toggleClass("addsvg2");
+		// $(this).find("svg").hide();
+
 	})
 
 	$(".ul > li").click(function(){
 		$(".ul > li").removeClass("active");
 		$(".ul > li").removeClass("open");
-		$(this).addClass("active"); 
+		$(this).addClass("active");
 	});
 
 	
 	$(".main_bar").click(function(){
 		 $(".main-left").toggleClass("left_min");
 		 $(".main-right").toggleClass("right_min");
+		 $(".child>ul>li>a").toggleClass("goLR");
 	})
 
-	$(document).on("mousemove",".left_min > ul >.child",function(e){
+	$(document).on("mousemove",".left_min > ul",function(e){
 		 $(".main-left").addClass("left_min1");
 		 $(".main-right").addClass("right_min1");
+		 $(".child>ul>li>a").removeClass("goLR");
 	})
-	$(document).on("mouseout",".left_min > ul >.child",function(e){
+	$(document).on("mouseout",".left_min > ul",function(e){
 		 $(".main-left").removeClass("left_min1");
 		 $(".main-right").removeClass("right_min1");
+		 $(".child>ul>li>a").addClass("goLR");
 	})
 });
--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express'});
 });
 
 router.get('/icon', function(req, res, next) {
-  res.render('icon', { title: 'Express' });
+  res.render('icon', { title: 'Express'});
 });
+
+router.get('/button', function(req, res, next) {
+  res.render('button', { title: 'Express'});
+});
+
 
 module.exports = router;
